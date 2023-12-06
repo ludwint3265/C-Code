d7ca3b186006ad1b4be88769d759a50608a11ac7
Ludwin Tenezaca <146135909+ludwint3265@users.noreply.github.com>
2023-12-06 12:47:53 -0500

    Add files via upload

---
+++ hw54.cpp
@@ -0,0 +1,17 @@
+//Name: Ludwin Tenezaca\
+//Date: 12/6/2023\
+//Email: ludwin.tenezaca38@myhunter.cuny.edu
+//This program writes 'Mihi cura futuri' 10 times
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ int i;
+ for (i=0; i<10; i++)
+ {
+  cout << "Mihi cura futuri\n";
+ }
+ return 0;
+}
